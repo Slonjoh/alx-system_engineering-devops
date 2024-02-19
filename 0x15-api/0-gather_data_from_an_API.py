ce0dcff3ab5118821e06ba67d47f90d6203e3321
Slonjoh <tobson11122@gmail.com>
2024-02-19 12:11:56 -0800

    api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+
+    user_info = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                             .format(employee_id)).json()
+    if 'name' not in user_info:
+        print("Employee with ID {} not found".format(employee_id))
+        sys.exit(1)
+
+    todo_list = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'
+        .format(employee_id)).json()
+
+    total_tasks = len(todo_list)
+    completed_tasks = sum(1 for task in todo_list if task['completed'])
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user_info['name'], completed_tasks, total_tasks))
+    for task in todo_list:
+        if task['completed']:
+            print("\t{}".format(task['title']))
