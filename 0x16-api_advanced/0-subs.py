9555044737595cec1164d9eb42f4516ecf8f070f
Slonjoh <tobson11122@gmail.com>
2024-03-05 04:38:03 -0800

    redit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,17 +7,15 @@ def number_of_subscribers(subreddit):
     """
     Uses the reddit api to get the numbers of subscribers to a subreddit
     """
-    subRcheck = requests.get("https://reddit.com/api/search_reddit_names.json",
-                             headers={
-                                 'User-Agent': 'Safari 12.1'
-                             },
-                             params={
-                                 'exact': True,
-                                 'query': subreddit
-                             })
-    if 'error' in subRcheck.json():
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    # Custom User-Agent to prevent Too Many Requests error
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-    response = requests.get("https://reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={'User-Agent': 'Safari 12.1'})
-    return response.json().get('data').get('subscribers')
